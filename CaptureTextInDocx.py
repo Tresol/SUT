49154c10f58c10ab45de67f16bad09a5d57a4315
Tresol <109974212+Tresol@users.noreply.github.com>
2023-04-23 11:34:13 +0800

    Update CaptureTextInDocx.py

--- CaptureTextInDocx.py
+++ CaptureTextInDocx.py
@@ -1,12 +1,11 @@
 import docx
-import time
+# Please install python-docx package first using the command "pip install python-docx".
+# import time
 from docx import Document
 
-log=open('log.txt', mode='a', encoding='utf-8')
+result=open('result.txt', mode='a', encoding='utf-8')
 
-for i in range(1,38):
-    filename="G:\LOST DIR\\第二轮\\o\\word\\text ("+str(i)+").docx"
-    file=docx.Document(filename)
-    for para in file.paragraphs:
-        print(para.text, file=log)
-    print("Success in"+filename)
+filename="G:\LOST DIR"+str(i)+").docx"
+file=docx.Document(filename)
+for para in file.paragraphs:
+    print(para.text, file=log)
